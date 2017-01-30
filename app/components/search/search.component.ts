43d5d17402d3075093d10662db19a552733997bb
Eran Krakovsky <ekrako@gmail.com>
2017-01-30 22:51:28 +0200

    finished chapter 15

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   moduleId: module.id,
   selector: 'search',
   templateUrl: `search.component.html`,
 })
-export class SearchComponent  { 
+export class SearchComponent {
+  searchStr: string;
+  constructor(private _spotifySerive: SpotifyService) {
+
+  }
+  searchMusic() {
+   this._spotifySerive.searchMusic(this.searchStr).subscribe(res =>{
+     console.log(res.artists.items);
+   });
+  }
 
 }
